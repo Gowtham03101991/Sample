a44fddd6aba5a77b93708ca303ade59b45895dc8
gowtham03101991 <gowthamtesting2022@gmail.com>
2022-01-26 13:20:23 +0530

    updates added

---
+++ src/information/Update.java
@@ -0,0 +1,13 @@
+package information;
+
+public class Update {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Updted lines added 1");
+		
+		System.out.println("Updted lines added 2");
+		
+	}
+
+}
